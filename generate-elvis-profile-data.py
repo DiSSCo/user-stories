a85c020cdecbb4bcaad1c893e317be8173f9b4cc
Sharif Islam <sharifislam@LT006684.local>
2019-07-18 23:19:10 +0200

    add identifiedBy

--- generate-elvis-profile-data.py
+++ generate-elvis-profile-data.py
@@ -33,13 +33,14 @@ def main():
  idf  = data["@reverse"]['identified']
  i=0
  while i < len(idf):
+      identifiedBy    = idf[i]['identifiedBy']
       occurrenceID    = idf[i]['occurrenceID']
       institutionCode = idf[i]['institutionCode']
       catalogNumber   = idf[i]['catalogNumber']
       scientificName  = idf[i]['scientificName']
       gbif            = idf[i]['sameAs']
       # PRINT 
-      print scientificName,occurrenceID,institutionCode,catalogNumber,gbif
+      print identifiedBy,scientificName,occurrenceID,institutionCode,catalogNumber,gbif
       i += 1 
 
 if __name__ == '__main__':
